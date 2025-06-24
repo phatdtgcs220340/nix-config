380ad5c6469343f0c646ce3862abe958e4e0c8ad
Đỗ Tấn Phát <phatdtgcs220340@fpt.edu.vn>
2025-06-24 14:50:06 +0700

    neovim config for extraPackages to ensure work well with lsp

--- modules/working/workflow/neovim.nix
+++ modules/working/workflow/neovim.nix
@@ -1,7 +1,10 @@
 # My config for neovim package and its related dependencies
-{ config, pkgs, lib, ... }:
-
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   options = {
     working.workflow.neovim.enable = lib.mkEnableOption "Enable Neovim with language servers";
   };
@@ -9,15 +12,32 @@
   config = lib.mkIf config.working.workflow.neovim.enable {
     programs.neovim = {
       enable = true;
+      extraPackages = with pkgs; [
+        # Language servers
+        nil
+        lua-language-server
+        pyright # Python
+        yaml-language-server
+        bash-language-server
+        jdt-language-server
+        nodejs
+        typescript
+        typescript-language-server
+        tailwindcss-language-server
+
+        # Formatters (optional)
+        black # Python formatter
+        shfmt # Bash formatter
+        yamllint # YAML linter
+        alejandra # nix formatter
+        prettier
+        eslint
+      ];
     };
 
     home.packages = with pkgs; [
-      lua-language-server
       jdt-language-server
-      nil              # nix language server
-      nixpkgs-fmt      # nix formatter
       (pkgs.vimPlugins.packer-nvim)
     ];
   };
 }
-
